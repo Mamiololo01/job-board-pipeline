7a59daeebf837bfd533e7742c562d997e218202e
oluwasayodev <oluwasayo.dev@gmail.com>
2023-04-30 00:56:34 +0100

    modified lambda handlers

--- handlers/load_csv_to_redshift.py
+++ handlers/load_csv_to_redshift.py
@@ -4,25 +4,26 @@ import dotenv
 
 config = dotenv.dotenv_values()
 
+
 def handler(event, context):
-    if isinstance(event, str):
-        event = json.loads(event)
+    try:
+        if isinstance(event, str):
+            print("Its a string: ", event)
+            event = json.loads(event)
 
-    bucket = event.get('Bucket')
-    key = event.get('Key')
-    region = config.get('AWS_REGION')
-    iam_role = config.get('REDSHIFT_IAM_ROLE')
+        bucket = event.get("Bucket")
+        key = event.get("Key")
+        region = config.get("REGION")
+        iam_role = config.get("REDSHIFT_IAM_ROLE")
 
-    con = helpers.get_database_conn()
-    res = helpers.load_csv_from_s3_to_redshift(
-        bucket=bucket,
-        key=key,
-        region=region,
-        iam_role=iam_role,
-        con=con
-    )
+        con = helpers.get_database_conn()
+        res = helpers.load_csv_from_s3_to_redshift(
+            bucket=bucket, key=key, region=region, iam_role=iam_role, con=con
+        )
 
-    return {
-        'result': f"s3://{config.get('TRANSFORMED_DATA_BUCKET')}/{event.get('Key')} sucessfully loaded to",
-        'res': res
-    }
+        return {
+            "result": f"s3://{config.get('TRANSFORMED_DATA_BUCKET')}/{event.get('Key')}",
+            "status": "Successful",
+        }
+    except Exception as e:
+        raise e("An error occured while loading to redshift")
--- handlers/transform_json_to_csv.py
+++ handlers/transform_json_to_csv.py
@@ -7,14 +7,15 @@ config = dotenv.dotenv_values()
 
 def handler(event, context):
     if isinstance(event, str):
-        event = event.replace("\'", "\"")
+        event = event.replace("'", '"')
         event = json.loads(event)
-    
+
     bucket = event.get("Bucket")
     key = event.get("Key")
-    filepath = get_file_from_s3(bucket=bucket, key=key, path_to_save='/tmp')
-    transformed_result = transform_data(filepath=filepath, path_to_save='/tmp')
+    filepath = get_file_from_s3(bucket=bucket, key=key, path_to_save="/tmp")
+    transformed_result = transform_data(filepath=filepath, path_to_save="/tmp")
     result = upload_file_to_s3(
-        filepath=transformed_result["output_path"], bucket=config.get("TRANSFORMED_DATA_BUCKET")
+        filepath=transformed_result["output_path"],
+        bucket=config.get("TRANSFORMED_DATA_BUCKET"),
     )
-    return {'create_table_stmnt': transformed_result['create_table_stmnt'], **result}
+    return {"create_table_stmnt": transformed_result["create_table_stmnt"], **result}
